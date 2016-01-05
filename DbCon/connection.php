8c132e9ed30f763528798d37072d00bdb76b3626
mukesh <mukesh@mukesh-PC>
2016-01-05 21:28:51 +0530

    comitt for desktop

--- DbCon/connection.php
+++ DbCon/connection.php
@@ -1,8 +1,6 @@
 
 <?php
 $conn = mysqli_connect("localhost","root", "064818@@muk","OfferDekho") or die("Could not Connect My Sql");
-//mysql_select_db("test",$con)  or die("Could connect to Database");
-
 if (mysqli_connect_errno()) {
     echo"failed to connect :" . mysqli_connect_errno();
 }
